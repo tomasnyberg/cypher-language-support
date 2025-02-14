d7f8fe65800fdc21649b97cb0ec8573c16dbfcd4
tomasnyberg <tomas.nyberg7335@gmail.com>
2025-02-14 13:49:13 +0100

    remove the ones that parser can't parse

--- packages/language-support/src/formatting/verify.ts
+++ packages/language-support/src/formatting/verify.ts
@@ -1,44 +1,51 @@
+import { CharStreams, CommonTokenStream } from 'antlr4';
 import * as fs from 'fs';
 import * as path from 'path';
+import CypherCmdLexer from '../generated-parser/CypherCmdLexer';
+import CypherCmdParser from '../generated-parser/CypherCmdParser';
 import { formatQuery } from "./formatting";
+import {
+  FormatterErrorsListener,
+} from './formattingHelpers';
 
 const filePath = path.join(__dirname, 'queries_10000.json');
 const fileContent = fs.readFileSync(filePath, 'utf-8');
 
 const queries: string[] = JSON.parse(fileContent);
+let badQueries = 0;
 let successful = 0;
-const eightCharRegex = /"{1,2}([A-Za-z0-9]{8})"{1,2}\s*$/;
-let eightCharErrorCount = 0;
 const reasons = new Map<string, number>();
 for (const query of queries) {
+  try {
+    const inputStream = CharStreams.fromString(query);
+    const lexer = new CypherCmdLexer(inputStream);
+    const tokens = new CommonTokenStream(lexer);
+    const parser = new CypherCmdParser(tokens);
+    parser.removeErrorListeners();
+    parser.addErrorListener(new FormatterErrorsListener());
+    parser.statementsOrCommands();
+  } catch (e) {
+    badQueries++;
+    continue;
+  }
   try {
     formatQuery(query);
     successful++;
   } catch (e) {
     let reason = e.message.split('near ')[1];
-    const test = typeof e.message === 'string' && eightCharRegex.test(e.message)
-     || reason === '"trim"';
-    if (test) {
-      reason = 'Eight char error';
-    }
     reasons.set(reason, (reasons.get(reason) || 0) + 1);
-    if (test) {
-      eightCharErrorCount++;
-    } else {
-      if (!test) {
-        console.log("\n-------------------\n");
-        console.log(e.message);
-        console.log("\n");
-        console.log(query);
-        console.log("\n");
-      }
-    }
+    console.log("\n-------------------\n");
+    //console.log(e);
+    console.log(e.message);
+    console.log("\n");
+    console.log(query);
+    console.log("\n");
   }
 }
 
-console.log(`Successfully formatted ${successful} queries out of ${queries.length}`);
-console.log(`Not including eight char errors, we successfully formatted: ${successful+eightCharErrorCount} queries out of ${queries.length}`);
-console.log(`Eight char error count: ${eightCharErrorCount}`);
+const goodQueries = queries.length - badQueries;
+
+console.log(`Successfully formatted ${successful} queries out of ${goodQueries}`);
 
 console.log("Reasons for errors, ascending order:");
 // Print reasons in ascending order
