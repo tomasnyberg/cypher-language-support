18d21bd2c51d7df65d19a5585b3cfd59e3566f55
tomasnyberg <tomas.nyberg7335@gmail.com>
2025-02-14 14:14:50 +0100

    add verifyformatting check

--- packages/language-support/src/formatting/verify.ts
+++ packages/language-support/src/formatting/verify.ts
@@ -7,6 +7,26 @@ import { formatQuery } from "./formatting";
 import {
   FormatterErrorsListener,
 } from './formattingHelpers';
+import { standardizeQuery } from './standardizer';
+
+function verifyFormatting(query: string): void {
+  const formatted = formatQuery(query);
+  const queryStandardized = standardizeQuery(query);
+  const formattedStandardized = standardizeQuery(formatted);
+  // AST integrity check
+  if (formattedStandardized !== queryStandardized) {
+    throw new Error(
+      `Standardized query does not match standardized formatted query`,
+    );
+  }
+  // Idempotency check
+  const formattedTwice = formatQuery(formatted);
+  if (formattedTwice !== formatted) {
+    throw new Error(
+      `Formatting is not idempotent`,
+    );
+  }
+}
 
 const filePath = path.join(__dirname, 'queries_10000.json');
 const fileContent = fs.readFileSync(filePath, 'utf-8');
@@ -29,13 +49,13 @@ for (const query of queries) {
     continue;
   }
   try {
-    formatQuery(query);
+    verifyFormatting(query);
     successful++;
   } catch (e) {
     let reason = e.message.split('near ')[1];
     reasons.set(reason, (reasons.get(reason) || 0) + 1);
     console.log("\n-------------------\n");
-    //console.log(e);
+    console.log(e);
     console.log(e.message);
     console.log("\n");
     console.log(query);
@@ -44,13 +64,4 @@ for (const query of queries) {
 }
 
 const goodQueries = queries.length - badQueries;
-
 console.log(`Successfully formatted ${successful} queries out of ${goodQueries}`);
-
-console.log("Reasons for errors, ascending order:");
-// Print reasons in ascending order
-const sortedReasons = Array.from(reasons.entries()).sort((a, b) => a[1] - b[1]);
-for (const [reason, count] of sortedReasons) {
-  console.log(`${reason}: ${count}`);
-}
-
